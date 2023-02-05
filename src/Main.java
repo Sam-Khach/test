da14f48828c04783dae553b9caf672c05725cbb6
SamsungCampus-04 <Samvel.k15.y@tumo.org>
2023-02-05 17:28:54 +0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+    static class Page {
+        String url;
+        Integer viewCount;
+
+        public Page(String url, Integer viewCount) {
+            this.url = url;
+            this.viewCount = viewCount;
+        }
+
+        @Override
+        public String toString() {
+            return "url='" + url + '\'' + ", viewCount=" + viewCount + '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Page> stack = new Stack<>();
+        stack.push(new Page("link1", 9));
+        stack.push(new Page("link2", 4));
+        stack.push(new Page("link3", 5));
+        stack.push(new Page("link4", 6));
+        while (!stack.empty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
